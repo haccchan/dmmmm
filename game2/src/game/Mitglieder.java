130168e14ffae3c5047d9cf692d110fbc1fc0e99
haccchan~ <ptht0711@gmail.com>
2025-01-14 05:40:20 +0100

    .

--- game2/src/game/Mitglieder.java
+++ game2/src/game/Mitglieder.java
@@ -21,6 +21,7 @@ public class Mitglieder {
     }
 	public void Mitgliederentfernen() {
 	    if (mitglieder.length == 3) { 
+	    	ERDeaktivieren();
 	        Spieler[] neuteam = new Spieler[1]; 
 	        neuteam[0] = mitglieder[0];
 	        mitglieder = neuteam; 
@@ -28,6 +29,69 @@ public class Mitglieder {
 	    }
 	    
 	}
+	public void ERDeaktivieren() {
+		if (er.wasser && er.luft) {
+			System.out.println("Sprießen (+20% GES) ist aktiviert für alle Mitglieder!");
+            for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                    int heil = mitglieder[i].maxges * 20 / 100;
+                    mitglieder[i].ges -= heil;
+                }
+            }
+        }
+		if(er.feuer && er.luft) {
+			System.out.println("Brennen (+20% ANG) ist aktiviert für alle Mitglieder!");
+			for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                    int starker = mitglieder[i].ang * 20 / 100;
+                    mitglieder[i].ang -= starker;
+                }
+            }
+		}
+		if(er.erde && er.luft) {
+			System.out.println("Beben (+20% VER) ist aktiviert für alle Mitglieder!");
+			for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                    int schild = mitglieder[i].ver * 20 / 100;
+                    mitglieder[i].ver -= schild;
+                }
+            }
+		}
+		if (er.feuer && er.wasser) {
+			System.out.println("Verdampfen (+10% GES, +10% ANG) ist aktiviert für alle Mitglieder!");
+            for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                    int heil = mitglieder[i].maxges * 10 / 100;
+                    mitglieder[i].ges += heil;
+                    int starker = mitglieder[i].ang * 20 / 100;
+                    mitglieder[i].ang -= starker;
+                }
+            }
+        }
+		if (er.wasser && er.erde) {
+			System.out.println("Tropfen (+10% GES, +10% VER) ist aktiviert für alle Mitglieder!");
+            for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                    int heil = mitglieder[i].maxges * 10 / 100;
+                    mitglieder[i].ges -= heil;
+                    int schild = mitglieder[i].ver * 10 / 100;
+                    mitglieder[i].ver -= schild;
+                }
+            }
+        }
+		if (er.feuer && er.erde) {
+			System.out.println("Überladen (+10% ANG, +10% VER) ist aktiviert für alle Mitglieder!");
+            for (int i = 0; i < mitglieder.length; i++) {
+                if (mitglieder[i] != null && mitglieder[i].Leben()) {
+                	int starker = mitglieder[i].ang * 20 / 100;
+                	mitglieder[i].ang -= starker;
+                    int schild = mitglieder[i].ver * 10 / 100;
+                    mitglieder[i].ver -= schild;
+                }
+            }
+        }
+	}
+	
 	public Spieler getMC(){
 		return mitglieder[0];
 	}
@@ -43,8 +107,8 @@ public class Mitglieder {
             }
         }
 		if(er.feuer && er.luft) {
+			System.out.println("Brennen (+20% ANG) ist aktiviert für alle Mitglieder!");
 			for (int i = 0; i < mitglieder.length; i++) {
-				System.out.println("Brennen (+20% ANG) ist aktiviert für alle Mitglieder!");
                 if (mitglieder[i] != null && mitglieder[i].Leben()) {
                     int starker = mitglieder[i].ang * 20 / 100;
                     mitglieder[i].ang += starker;
